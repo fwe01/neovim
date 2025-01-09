d36429f65566151a825f3039f3e01071ebdb3c25
Frederick William Edlim <frederickwilliamedlim@gmail.com>
2025-01-09 23:01:49 +0700

    setting tab 4 spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+opt.winbl = 10
+opt.colorcolumn = "72"
+opt.scrolloff = 18
+opt.foldmethod = "marker"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+opt.shiftwidth = 4
