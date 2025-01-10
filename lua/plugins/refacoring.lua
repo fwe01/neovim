720ce556b910775e44d841197637ce08b133c8ed
Frederick William Edlim <frederickwilliamedlim@gmail.com>
2025-01-10 15:23:03 +0700

    update lsp config

--- lua/plugins/refacoring.lua
+++ lua/plugins/refacoring.lua
@@ -7,18 +7,46 @@ return {
   config = function()
     require("refactoring").setup()
 
-    vim.keymap.set("x", "<leader>re", function() require('refactoring').refactor('Extract Function') end)
-    vim.keymap.set("x", "<leader>rf", function() require('refactoring').refactor('Extract Function To File') end)
+    vim.keymap.set("x", "<leader>ref", function()
+      require("refactoring").refactor("Extract Function")
+    end, {
+      desc = "Extract Function",
+    })
+    vim.keymap.set("x", "<leader>reF", function()
+      require("refactoring").refactor("Extract Function To File")
+    end, {
+      desc = "Extract Function To File",
+    })
     -- Extract function supports only visual mode
-    vim.keymap.set("x", "<leader>rv", function() require('refactoring').refactor('Extract Variable') end)
+    vim.keymap.set("x", "<leader>rev", function()
+      require("refactoring").refactor("Extract Variable")
+    end, {
+      desc = "Extract Variable",
+    })
     -- Extract variable supports only visual mode
-    vim.keymap.set("n", "<leader>rI", function() require('refactoring').refactor('Inline Function') end)
+    vim.keymap.set("n", "<leader>rif", function()
+      require("refactoring").refactor("Inline Function")
+    end, {
+      desc = "Inline Function",
+    })
     -- Inline func supports only normal
-    vim.keymap.set({ "n", "x" }, "<leader>ri", function() require('refactoring').refactor('Inline Variable') end)
+    vim.keymap.set({ "n", "x" }, "<leader>riv", function()
+      require("refactoring").refactor("Inline Variable")
+    end, {
+      desc = "Inline Variable",
+    })
     -- Inline var supports both normal and visual mode
 
-    vim.keymap.set("n", "<leader>rb", function() require('refactoring').refactor('Extract Block') end)
-    vim.keymap.set("n", "<leader>rbf", function() require('refactoring').refactor('Extract Block To File') end)
+    vim.keymap.set("n", "<leader>reb", function()
+      require("refactoring").refactor("Extract Block")
+    end, {
+      desc = "Extract Block",
+    })
+    vim.keymap.set("n", "<leader>reB", function()
+      require("refactoring").refactor("Extract Block To File")
+    end, {
+      desc = "Extract block to file",
+    })
     -- Extract block supports only normal mode
   end,
 }
