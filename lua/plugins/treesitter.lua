1b48b00a4f2900ba6f27135b205bae65ea9fa8ab
Frederick William Edlim <frederickwilliamedlim@gmail.com>
2025-01-24 14:50:01 +0700

    sudah makin enak bos

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+        ensure_installed = {
+            "bash",
+            "html",
+            "javascript",
+            "json",
+            "lua",
+            "markdown",
+            "php",
+            "markdown_inline",
+            "python",
+            "query",
+            "regex",
+            "tsx",
+            "typescript",
+            "vim",
+            "yaml",
+        },
+    },
+}
