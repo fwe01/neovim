01fc00fcdf262efca405fc05f8e072b277f705bd
Frederick William Edlim <frederickwilliamedlim@gmail.com>
2025-05-20 08:09:12 +0700

    backup 20 05 2025

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -32,13 +32,13 @@ return {
   },
 
   -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-  { import = "lazyvim.plugins.extras.lang.typescript" },
+  -- { import = "lazyvim.plugins.extras.lang.typescript" },
 
   -- use mini.starter instead of alpha
-  { import = "lazyvim.plugins.extras.ui.mini-starter" },
+  -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
 
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-  { import = "lazyvim.plugins.extras.lang.json" },
+  -- { import = "lazyvim.plugins.extras.lang.json" },
 
   -- add any tools you want to have installed below
   {
